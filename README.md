cc18d62bf4a699da817221c6cc2d1a25a9d4eb6f
Jamie Pezone <jampez77@gmail.com>
2021-03-25 14:48:52 +0000

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # Multiline Entity Card
- A custom entity card for Home Assistant that allows text to span multiple lines.
+ A custom entity card for [Home Assistant](https://www.home-assistant.io/) that allows text to span multiple lines.
+
+ <img src='card.png' />
+
+ [![GitHub Release][releases-shield]][releases]
+ ![Project Maintenance][maintenance-shield]
+ [![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
+
+ ## Installation through [HACS](https://github.com/custom-components/hacs)
+
+ Use [HACS](https://github.com/custom-components/hacs) to install the **Multiline Entity Card** plugin.
+
+ ## Configuration
+
+ ```yaml
+ type: 'custom:multiline-entity-card'
+ ```
+
+ ### Example:
+ ```yaml
+ type: 'custom:multiline-entity-card'
+ name: "Master Bedroom"
+ entity: sensor.master_bedroom_reminders
+ ```
+
+ [commits-shield]: https://img.shields.io/github/commit-activity/y/jampez77/multiline-entity-card.svg?style=for-the-badge
+ [commits]: https://github.com/jampez77/multiline-entity-card/commits/master
+ [license-shield]: https://img.shields.io/github/license/jampez77/multiline-entity-card.svg?style=for-the-badge
+ [maintenance-shield]: https://img.shields.io/badge/Maintainer-Jamie%20Nandhra--Pezone-blue
+ [releases-shield]: https://img.shields.io/github/release/jampez77/multiline-entity-card.svg?style=for-the-badge
+ [releases]: https://github.com/jampez77/multiline-entity-card/releases
---
+++ multiline-entity-card.js
@@ -0,0 +1,138 @@
+const LitElement = customElements.get("hui-masonry-view") ? Object.getPrototypeOf(customElements.get("hui-masonry-view")) : Object.getPrototypeOf(customElements.get("hui-view"));
+const html = LitElement.prototype.html;
+const css = LitElement.prototype.css;
+
+const fireEvent = (node, type, detail, options) => {
+  options = options || {};
+  detail = detail === null || detail === undefined ? {} : detail;
+  const event = new Event(type, {
+    bubbles: options.bubbles === undefined ? true : options.bubbles,
+    cancelable: Boolean(options.cancelable),
+    composed: options.composed === undefined ? true : options.composed,
+  });
+  event.detail = detail;
+  node.dispatchEvent(event);
+  return event;
+};
+window.customCards = window.customCards || [];
+
+class MultilineEntityCard extends LitElement {
+
+  static get properties() {
+    return {
+      config: {},
+      _hass: {},
+    };
+  }
+
+    set hass(hass) {
+      this._hass = hass;
+    }
+
+    setConfig(config) {
+      if (!config.entity) {
+        throw new Error('You need to define an entity');
+      }
+      this.config = config;
+    }
+
+    // The height of your card. Home Assistant uses this to automatically
+    // distribute all cards over the available columns.
+    getCardSize() {
+      return 1;
+    }
+
+    render() {
+      if (!this.config || !this._hass) {
+        return html``;
+      }
+
+      this.numberElements = 0;
+
+      const state = this._hass.states[this.config.entity];
+
+      if (!state) {
+        return html`
+          <style>
+            .not-found {
+              flex: 1;
+              background-color: yellow;
+              padding: 8px;
+            }
+          </style>
+          <ha-card>
+            <div class="not-found">
+              Entity not available: ${this.config.entity}
+            </div>
+          </ha-card>
+        `;
+      }
+
+      return html`
+        <ha-card @click="${this._handleClick}">
+
+        <div class="header">
+          <div class="name">
+          ${this.config.name == 0 ?
+            state.attributes.friendly_name :
+            this.config.name
+          }
+          </div>
+          <ha-icon icon="${state.attributes.icon == undefined ?
+            "mdi:eye" :
+            state.attributes.icon
+          }"></ha-icon>
+        </div>
+        <div class="info">
+          <span class="value">
+            ${state ? state.state : 'unavailable'}
+          </span>
+        </div>
+        </ha-card>
+      `;
+    }
+
+    _handleClick() {
+      fireEvent(this, "hass-more-info", { entityId: this.config.entity });
+    }
+
+    static get styles() {
+      return css`
+      ha-card {
+        cursor: pointer;
+        padding: 0 16px 0;
+      }
+      .header{
+        padding: 8px 16px 0px;
+        display: flex;
+        justify-content: space-between;
+      }
+      .name{
+        color: var(--secondary-text-color);
+        line-height: 40px;
+        font-weight: 500;
+        font-size: 16px;
+        overflow: hidden;
+        white-space: nowrap;
+        text-overflow: ellipsis;
+      }
+      ha-icon{
+        color: var(--state-icon-color, #44739e);
+        line-height: 40px;
+      }
+      .info{
+        padding: 0px 16px 16px;
+        margin-top: -4px;
+        overflow: hidden;
+        line-height: 28px;
+      }
+      .value{
+        font-size: 2vw;
+        margin-right: 4px;
+      }
+      `;
+    }
+
+  }
+
+  customElements.define('multiline-entity-card', MultilineEntityCard);
